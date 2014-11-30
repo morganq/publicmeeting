6da3d551fb8ab33b602f013dbed69a258897eeac
Morgan Quirk <morganquirk@gmail.com>
2014-11-30 13:36:06 -0800

    css prettiness

--- publicmeeting/models.py
+++ publicmeeting/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Registration(models.Model):
 	first_name = models.CharField(max_length=64)
-	email_address = models.EmailField()
-	availability = models.CharField(max_length=256)
+	email_address = models.EmailField(verbose_name="Email Address (hidden)")
+	availability = models.CharField(max_length=256, verbose_name="Availability (for instance: \"after 5pm weekdays\")")
--- publicmeeting/views.py
+++ publicmeeting/views.py
@@ -5,13 +5,15 @@ from publicmeeting.forms import *
 
 
 def home(request):
+	registrations = Registration.objects.order_by('first_name')
 	if request.method=='POST':
 		form = RegistrationForm(request.POST)
 		if form.is_valid():
 			form.save()
 			return HttpResponseRedirect("/")
+		else:
+			return render(request, "home.html", {"registrations":registrations, "form":form})
 	else:
-		registrations = Registration.objects.order_by('first_name')
 		form = RegistrationForm()
 		print form
 		return render(request, "home.html", {"registrations":registrations, "form":form})
